4bf80f2cc093630541a6e152321f03f92f3631ef
Михаил <mxoilslukin@gmail.com>
2025-03-19 00:19:50 +0500

    CARD-69: review fixes

--- tests/services/cars/test_update_score.py
+++ tests/services/cars/test_update_score.py
@@ -2,19 +2,20 @@ from decimal import Decimal
 from uuid import uuid4
 
 import pytest
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.errors.service import CarNotFoundError
 from src.services.car import CarService
 from tests.factories.car import CarFactory
 
 
-async def test_update_score_ok(car_service: CarService) -> None:
+async def test_update_score_ok(car_service: CarService, session: AsyncSession) -> None:
     car = await CarFactory.create()
     await car_service.update_score(car.id, Decimal('4.0'))
-    assert True
+    await session.refresh(car)
+    assert car.score == Decimal('4.0')
 
 
 async def test_update_score_nf(car_service: CarService) -> None:
     with pytest.raises(CarNotFoundError):
         await car_service.update_score(uuid4(), Decimal('4.0'))
-    assert True
