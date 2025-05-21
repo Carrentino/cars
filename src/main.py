74ea32c534de917b1a02440e7c58c968a3363a8c
Михаил <mxoilslukin@gmail.com>
2025-05-21 18:14:37 +0500

    some fixes

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ from src.settings import get_settings
 
 def main() -> None:
     uvicorn.run(
-        # app='main:make_app',
-        app=make_app,
-        # workers=get_settings().workers_count,
+        app='src.bootstrap:make_app',
+        # app=make_app,
+        workers=6,
         host=get_settings().host,
         port=get_settings().port,
         # reload=get_settings().reload,
